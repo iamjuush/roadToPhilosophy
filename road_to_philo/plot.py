91128d3311c5d9e9a7ac2e7d939fcc707a6066c3
Joshua Leong <juushdev@gmail.com>
2019-09-13 17:22:38 +0800

    D3 graph added.

--- road_to_philo/plot.py
+++ road_to_philo/plot.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from os import path
 
 DATA_PATH = path.join(path.dirname(path.dirname(__file__)), 'crawlerData')
+D3_PATH = path.join(path.dirname(__file__), 'd3')
 
 with open(DATA_PATH, 'rb') as f:
     g = pickle.load(f)
@@ -16,6 +17,6 @@ nx.spring_layout(g, k=0.15/math.sqrt(g.order()), iterations=20)
 nx.draw(g, with_labels=True, node_color='#ADD8E6')
 
 data = json_graph.node_link_data(g)
-with open('graph.json', 'w') as f:
+with open(path.join(D3_PATH, 'graph.json'), 'w') as f:
     json.dump(data,f)
 plt.show()
