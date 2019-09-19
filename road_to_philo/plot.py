9ea05d908e8338b89c7a68e239e444abf5a6f22c
Joshua Leong <juushdev@gmail.com>
2019-09-19 08:09:44 +0800

    Improve graphing abilities

--- road_to_philo/plot.py
+++ road_to_philo/plot.py
@@ -12,9 +12,10 @@ D3_PATH = path.join(path.dirname(__file__), 'd3')
 with open(DATA_PATH, 'rb') as f:
     g = pickle.load(f)
 
-fig = plt.figure(figsize=[100,100])
-nx.spring_layout(g, k=0.15/math.sqrt(g.order()), iterations=20)
-nx.draw(g, with_labels=True, node_color='#ADD8E6')
+nx.write_gexf(g, 'graph.gexf')
+# fig = plt.figure(figsize=[100,100])
+# nx.spring_layout(g, k=0.15/math.sqrt(g.order()), iterations=20)
+# nx.draw(g, with_labels=True, node_color='#ADD8E6')
 
 data = json_graph.node_link_data(g)
 with open(path.join(D3_PATH, 'graph.json'), 'w') as f:
